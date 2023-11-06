e74d61f9ad5ff5f96083fb4c8da627ceb1a92c86
Louis Chou <93809518+LouisYuHung@users.noreply.github.com>
2023-11-06 10:51:03 +0800

    Update api.php

--- api.php
+++ api.php
@@ -2,19 +2,19 @@
 $exchangeRate = json_decode('{
     "currencies": {
         "TWD": {
-        "TWD": 1,
-        "JPY": 3.669,
-        "USD": 0.03281
+            "TWD": 1,
+            "JPY": 3.669,
+            "USD": 0.03281
         },
         "JPY": {
-        "TWD": 0.26956,
-        "JPY": 1,
-        "USD": 0.00885
+            "TWD": 0.26956,
+            "JPY": 1,
+            "USD": 0.00885
         },
         "USD": {
-        "TWD": 30.444,
-        "JPY": 111.801,
-        "USD": 1
+            "TWD": 30.444,
+            "JPY": 111.801,
+            "USD": 1
         }
     }
 }', true);
